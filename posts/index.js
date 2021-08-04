7a0d7c120ed666cf4bd361a2ef3f008c84ef3e09
Iron.Snowflake <jadrubio@gmail.com>
2021-08-04 15:51:36 -0400

    added in cors checks

--- posts/index.js
+++ posts/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const { randomBytes } = require('crypto');
+const cors = require('cors');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 // since this is a small porject o play around with
 // microservices not going to set up a db and jut store info locally
